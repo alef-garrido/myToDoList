736163aca878f70b4cb6b58c7ce6f515da3bd815
Alef Garrido <oa.garrido11@gmail.com>
2021-08-25 10:15:07 -0500

    Add empty state message

--- src/index.js
+++ src/index.js
@@ -14,22 +14,34 @@ const bullets = module.Storage.getFromStorage();
 function renderList(arr) {
   const list = document.getElementById('listContainer');
   list.innerHTML = '';
-  arr.forEach((task, index) => {
-    const listContainer = document.getElementById('listContainer');
-    const taskRow = document.createElement('li');
-    const checkmark = document.createElement('input');
-    const text = document.createElement('p');
-    const dots = document.createElement('span');
+  if(arr.length === 0) {
+    const container = document.getElementById('listContainer');
+    const stateContainer = document.createElement('div');
+    const mssg = document.createElement('p');
+    mssg.textContent = 'What are your goals today? :)';
+    stateContainer.appendChild(mssg);
+    stateContainer.classList.add('emptyState')
+    container.appendChild(stateContainer)
+    
+  } else
+  {  
+    arr.forEach((task, index) => {
+      const listContainer = document.getElementById('listContainer');
+      const taskRow = document.createElement('li');
+      const checkmark = document.createElement('input');
+      const text = document.createElement('p');
+      const dots = document.createElement('span');
 
-    checkmark.setAttribute('type', 'checkbox');
-    checkmark.classList.add('status', 'form-check-input', 'me-2');
-    checkmark.checked = task.completed;
-    text.textContent = task.description;
-    dots.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-    taskRow.append(checkmark, text, dots);
-    taskRow.id = index + 1;
-    listContainer.appendChild(taskRow);
-  });
+      checkmark.setAttribute('type', 'checkbox');
+      checkmark.classList.add('status', 'form-check-input', 'me-2');
+      checkmark.checked = task.completed;
+      text.textContent = task.description;
+      dots.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+      taskRow.append(checkmark, text, dots);
+      taskRow.id = index + 1;
+      listContainer.appendChild(taskRow);
+    });
+  }
 }
 
 window.addEventListener('load', renderList(bullets));
