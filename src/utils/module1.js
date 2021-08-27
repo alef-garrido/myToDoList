0534226ec8c59c2e17c0a4552b20a27546254641
Alef Garrido <oa.garrido11@gmail.com>
2021-08-27 07:30:14 -0500

    Refactoring of renderList function

--- src/utils/module1.js
+++ src/utils/module1.js
@@ -30,35 +30,29 @@ function renderList(arr) {
   const list = document.getElementById('listContainer');
   list.innerHTML = '';
   if (arr.length === 0) {
-    const container = document.getElementById('listContainer');
-    const stateContainer = document.createElement('div');
-    const mssg = document.createElement('p');
-    mssg.textContent = 'What are your goals today? :)';
-    stateContainer.appendChild(mssg);
-    stateContainer.classList.add('emptyState');
-    container.appendChild(stateContainer);
+    const emptyState = document.getElementById('listContainer');
+    const str = `<div class="emptyState"> 
+      <p>What are your goals today? :)<p> 
+    </div>`
+    emptyState.insertAdjacentHTML('afterbegin', str);
+
   } else {
     arr.forEach((task, index) => {
       const listContainer = document.getElementById('listContainer');
-      const taskRow = document.createElement('li');
-      const checkmark = document.createElement('input');
-      const taskBody = document.createElement('input');
-      const dots = document.createElement('span');
+      const str = `<li id=${index + 1} class="task ${task.completed}">
+          <input type="checkbox" id=check-${index + 1} class="status form-check-input me-2">
+          <input type="text" id=task-${index + 1} class="todo" value='${task.description}'>
+          <span><i class="fas fa-ellipsis-v"></i></span>
+          <span class="hide"><i class="fas fa-trash-alt"></i></span>
+        </li>`
+      
+      listContainer.insertAdjacentHTML("beforeend", str)
 
-      checkmark.setAttribute('type', 'checkbox');
-      checkmark.classList.add('status', 'form-check-input', 'me-2');
-      checkmark.checked = task.completed;
-      taskBody.setAttribute('type', 'text');
-      taskBody.classList.add('todo');
-      taskBody.id = `task-${index+1}`
-      taskBody.value = task.description;
-      dots.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-      taskRow.append(checkmark, taskBody, dots);
-      taskRow.id = index + 1;
-      taskRow.classList.add('task', `${task.completed}` )
-      listContainer.appendChild(taskRow);
       if (task.completed === true) {
-        taskBody.classList.add('completed')
+        let finished = document.getElementById(`task-${index + 1}`)
+        let checks = document.getElementById(`check-${index + 1}`)
+        checks.toggleAttribute('checked')
+        finished.classList.toggle('completed')
       }
      });
   }
